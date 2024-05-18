85eb73396df058c293970a82bb101ec07fd18384
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-05-18 16:15:13 +0700

    Add pass test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,6 +19,6 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("something here"));
+                .body("data", equalTo("Hello"));
     }
 }
